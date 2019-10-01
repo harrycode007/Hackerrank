3e4f18eee2f02b16e6b3adb7d8fb12fab8e11b1e
Athi Dsh <32543864+Athi101@users.noreply.github.com>
2019-10-01 21:52:48 +0530

    Update day3.java

--- day3.java
+++ day3.java
@@ -10,11 +10,11 @@ public class Solution {
 
 
 
-    private static final Scanner scanner = new Scanner(System.in);
+    private static final Scanner sc = new Scanner(System.in);
 
     public static void main(String[] args) {
-        int N = scanner.nextInt();
-        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+        int N = sc.nextInt();
+        sc.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
         if(N % 2 == 1)
             System.out.println("Weird");
         else if(N % 2 == 0 && N>=2 && N<=5)
@@ -26,6 +26,6 @@ public class Solution {
         
         
 
-        scanner.close();
+        sc.close();
     }
 }
